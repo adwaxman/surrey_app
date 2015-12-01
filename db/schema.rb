d93fe1dd90912e3f6e526279adb0348d5db32d98
mikerob215 <mikerobinson1@gmail.com>
2015-12-01 12:27:10 -0500

    members controller almost complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124210338) do
+ActiveRecord::Schema.define(version: 20151201161214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20151124210338) do
     t.boolean  "active"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "mi"
   end
 
   create_table "origins", force: :cascade do |t|
