a6cee9f1169461e1750ca0074a8a68a3f11f4703
mikerob215 <mikerobinson1@gmail.com>
2015-12-07 17:46:05 -0500

    fix migration

--- db/migrate/20151206214621_driver_monday_max_string.rb
+++
@@ -1,8 +0,0 @@
-class DriverMondayMaxString < ActiveRecord::Migration
-  def change
-    change_table :drivers do |t|
-      t.change :monday_min, :time
-      t.change :monday_max, :time
-    end
-  end
-end
