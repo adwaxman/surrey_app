d6fefffb545e65a7c1926cb0088a4f7495df933d
mikerob215 <mikerobinson1@gmail.com>
2015-12-15 15:36:37 -0500

    heroku 8

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ module SurreyApp
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
 
+
+    config.assets.paths << Rails.root.join('vendor', 'assets')
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
