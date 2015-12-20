c98e7528f8c782d0a4db5807b92771681042f2eb
mikerob215 <mikerobinson1@gmail.com>
2015-12-20 14:39:32 -0500

    fix for admin update of member

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -27,6 +27,7 @@ class Admin::MembersController < ApplicationController
   def update
     @member = Member.find(params[:id])
     if @member.update_attributes(member_params)
+      @member.update(confirmed: true)
       Origin.create(member_id: @member.id, address_line1: @member.address_line1, address_line2: @member.address_line2, city: @member.city, state: @member.state, zip: @member.zip, primary: true)
       redirect_to members_path
     else
