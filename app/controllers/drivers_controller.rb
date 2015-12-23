8e2a43708aed3f717612980d9d4b7b901623b1a7
Adam Waxman <adwaxman@gmail.com>
2015-12-23 11:51:12 -0500

    move matching logic for new driver and existing open rides to update action since scheduled is no longer filled out at creation

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -2,7 +2,6 @@ class DriversController < ApplicationController
   before_action :driver?, except: [:show]
   before_action :admin?, only: [:show]
 
-
   def new
     @driver = Driver.new
   end
@@ -10,39 +9,10 @@ class DriversController < ApplicationController
   def create
     @driver = Driver.new(driver_params)
     @driver.county_preference = params[:driver][:county_preference]
-    @driver.county_preference.push("All")
+    @driver.county_preference.push('All')
     if @driver.save
       Matcher.create(driver_id: @driver.id)
-      @open_rides = Ride.all.where(status:"open")
-      @matches = []
-      @open_rides.each do |ride|
-          @weekday = Date.parse(ride.pickup_date).strftime('%A').downcase
-          if @weekday = "monday" && @driver.monday
-            if @driver.monday_min < ride.pickup_time && @driver.monday_max > ride.pickup_time
-              @matches.push(ride)
-            end
-          elsif @weekday = "tuesday" && @driver.tuesday
-            if @driver.tuesday_min < ride.pickup_time && @driver.tuesday_max > ride.pickup_time
-              @matches.push(ride)
-            end
-          elsif @weekday = "wednesday" && @driver.wednesday
-            if @driver.wednesday_min < ride.pickup_time && @driver.wednesday_max > ride.pickup_time
-              @matches.push(ride)
-            end
-          elsif @weekday = "thursday" && @driver.thursday
-            if @driver.thursday_min < ride.pickup_time && @driver.thursday_max > ride.pickup_time
-              @matches.push(ride)
-            end
-          elsif @weekday = "friday" && @driver.friday
-            if @driver.friday_min < ride.pickup_time && @driver.friday_max > ride.pickup_time
-              @matches.push(ride)
-            end
-          end
-      end
 
-      @matches.each do |ride|
-        Match.create(matcher_id: @driver.id, ride_id: ride.id)
-      end
       redirect_to root_path
     else
       render 'new'
@@ -50,7 +20,7 @@ class DriversController < ApplicationController
   end
 
   def index
-    @drivers = Driver.all.sort_by {|driver| driver.lname.downcase}
+    @drivers = Driver.all.sort_by { |driver| driver.lname.downcase }
 
     @arr_of_drivers = []
     @drivers.each do |driver|
@@ -59,40 +29,84 @@ class DriversController < ApplicationController
   end
 
   def edit
-
     @driver = Driver.find(params[:id])
 
     # Show time in readable format
-    @driver.monday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p") if @driver.monday_min
-    @driver.monday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p") if @driver.monday_max
+    @driver.monday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p') if @driver.monday_min
+    @driver.monday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p') if @driver.monday_max
 
-    @driver.tuesday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p") if @driver.tuesday_min
-    @driver.tuesday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p") if @driver.tuesday_max
+    @driver.tuesday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p') if @driver.tuesday_min
+    @driver.tuesday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p') if @driver.tuesday_max
 
-    @driver.wednesday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p") if @driver.wednesday_min
-    @driver.wednesday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p") if @driver.wednesday_max
+    @driver.wednesday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p') if @driver.wednesday_min
+    @driver.wednesday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p') if @driver.wednesday_max
 
-    @driver.thursday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p") if @driver.thursday_min
-    @driver.thursday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p") if @driver.thursday_max
+    @driver.thursday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p') if @driver.thursday_min
+    @driver.thursday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p') if @driver.thursday_max
 
-    @driver.friday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p") if @driver.friday_min
-    @driver.friday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p") if @driver.friday_max
+    @driver.friday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p') if @driver.friday_min
+    @driver.friday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p') if @driver.friday_max
   end
 
   def update
     @driver = Driver.find(params[:id])
     @driver.county_preference = params[:driver][:county_preference]
-    if @driver.update_attributes(driver_params)
-      @driver.update(confirmed: true)
-      if current_driver
-        redirect_to driver_driverpanel_path(@driver.id)
-      elsif current_admin
-        redirect_to driver_path(@driver)
-      end
-    else
-      render 'edit'
-    end
+    if @driver.confirmed.nil?
+      if @driver.update_attributes(driver_params)
+      
+        @open_rides = Ride.all.where(status: 'open')
+        @matches = []
+        @open_rides.each do |ride|
+          @weekday = Date.parse(ride.pickup_date).strftime('%A').downcase
+          if @weekday = 'monday' && @driver.monday
+            if @driver.monday_min < ride.pickup_time && @driver.monday_max > ride.pickup_time
+              @matches.push(ride)
+            end
+          elsif @weekday = 'tuesday' && @driver.tuesday
+            if @driver.tuesday_min < ride.pickup_time && @driver.tuesday_max > ride.pickup_time
+              @matches.push(ride)
+            end
+          elsif @weekday = 'wednesday' && @driver.wednesday
+            if @driver.wednesday_min < ride.pickup_time && @driver.wednesday_max > ride.pickup_time
+              @matches.push(ride)
+            end
+          elsif @weekday = 'thursday' && @driver.thursday
+            if @driver.thursday_min < ride.pickup_time && @driver.thursday_max > ride.pickup_time
+              @matches.push(ride)
+            end
+          elsif @weekday = 'friday' && @driver.friday
+            if @driver.friday_min < ride.pickup_time && @driver.friday_max > ride.pickup_time
+              @matches.push(ride)
+            end
+          end
+        end
 
+        @matches.each do |ride|
+          Match.create(matcher_id: @driver.id, ride_id: ride.id)
+        end
+
+        @driver.update(confirmed: true)
+
+        if current_driver
+          redirect_to driver_driverpanel_path(@driver.id)
+        elsif current_admin
+          redirect_to driver_path(@driver)
+        end
+      else
+        render 'edit'
+      end
+
+    else
+      if @driver.update_attributes(driver_params)
+        if current_driver
+          redirect_to driver_driverpanel_path(@driver.id)
+        elsif current_admin
+          redirect_to driver_path(@driver)
+        end
+      else
+        render 'edit'
+      end
+    end
   end
 
   def show
@@ -105,32 +119,31 @@ class DriversController < ApplicationController
     else
 
       # Show time in readable format
-      @monday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p")
-      @monday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p")
+      @monday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p')
+      @monday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p')
 
-      @tuesday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p")
-      @tuesday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p")
+      @tuesday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p')
+      @tuesday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p')
 
-      @wednesday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p")
-      @wednesday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p")
+      @wednesday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p')
+      @wednesday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p')
 
-      @thursday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p")
-      @thursday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p")
+      @thursday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p')
+      @thursday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p')
 
-      @friday_min = Time.parse(@driver.monday_min.to_s).strftime("%l:%M %p")
-      @friday_max = Time.parse(@driver.monday_max.to_s).strftime("%l:%M %p")
+      @friday_min = Time.parse(@driver.monday_min.to_s).strftime('%l:%M %p')
+      @friday_max = Time.parse(@driver.monday_max.to_s).strftime('%l:%M %p')
 
       @counties = @driver.county_preference
-      @upper = @counties.length-1
+      @upper = @counties.length - 1
 
-      @assigned_rides = @driver.rides.sort_by {|ride| DateTime.parse(ride.pickup_date)}
-      @matched_rides = Matcher.find(@driver.id).rides.where(status: "open").sort_by {|ride| DateTime.parse(ride.pickup_date)}
+      @assigned_rides = @driver.rides.sort_by { |ride| DateTime.parse(ride.pickup_date) }
+      @matched_rides = Matcher.find(@driver.id).rides.where(status: 'open').sort_by { |ride| DateTime.parse(ride.pickup_date) }
 
       @outreaches = @driver.outreaches
       @notes = @driver.notes
 
     end
-
   end
 
   def prompt
@@ -139,13 +152,13 @@ class DriversController < ApplicationController
 
   def inactive
     @driver = Driver.find(params[:driver_id])
-    if @driver.confirmed == nil
-      @driver.update(active: false, address_line1: "placeholder", city: "placeholder", state: "PA", zip: "placeholder")
+    if @driver.confirmed.nil?
+      @driver.update(active: false, address_line1: 'placeholder', city: 'placeholder', state: 'PA', zip: 'placeholder')
       redirect_to driver_path @driver
-    elsif @driver.update(active:false)
+    elsif @driver.update(active: false)
       redirect_to driver_path @driver
     else
-      flash[:alert] = "There was a problem."
+      flash[:alert] = 'There was a problem.'
       redirect :back
     end
   end
@@ -153,10 +166,10 @@ class DriversController < ApplicationController
   def reactivate
     @driver = Driver.find(params[:driver_id])
     if @driver.update(active: true)
-      flash[:alert] = "Driver has been reactivated"
+      flash[:alert] = 'Driver has been reactivated'
       redirect_to driver_path @driver
     else
-      flash[:alert] = "There was a problem."
+      flash[:alert] = 'There was a problem.'
       redirect :back
     end
   end
@@ -166,20 +179,20 @@ class DriversController < ApplicationController
     redirect_to driver_path @driver
   end
 
-private
+  private
 
   def admin?
     redirect_to admin_login_path unless current_admin
   end
+
   def driver_params
     params.require(:driver).permit(:fname, :lname, :email, :password, :password_confirmation, :address_line1, :address_line2, :city, :state, :zip, :car_type, :monday, :monday_min, :monday_max, :tuesday, :tuesday_min, :tuesday_max, :wednesday, :wednesday_min, :wednesday_max, :thursday, :thursday_min, :thursday_max, :friday, :friday_min, :friday_max, :unavailable, :accommodate_wheelchair, :accommodate_aide, :preferred_contact, :phone, :cell, :county_preference, :prefered_contact, :accommodate_pet)
   end
 
   def driver?
     unless current_driver || current_admin
-      flash[:notice] = "You must be logged in as a driver or administrator"
+      flash[:notice] = 'You must be logged in as a driver or administrator'
       redirect_to root_path
     end
   end
-
 end
