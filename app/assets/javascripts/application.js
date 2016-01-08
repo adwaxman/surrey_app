5948a7a1672711846bba479a0bfad8d956fbf639
Mike Robinson <mikerobinson1@gmail.com>
2016-01-08 15:12:01 -0500

    remove empty javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,8 @@
 
 //= require jquery
 //= require jquery_ujs
-
 //= require jquery.turbolinks
-
-
-
 //= require rides
-//= require drivers
 //= require turbolinks
 //= require jquery-readyselector
 //= require_tree .
