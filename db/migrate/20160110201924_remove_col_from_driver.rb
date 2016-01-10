822bbb5b6fa827add731424167950cedc7850c62
Mike Robinson <mikerobinson1@gmail.com>
2016-01-10 18:20:27 -0500

    fix migration

--- db/migrate/20160110201924_remove_col_from_driver.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColFromDriver < ActiveRecord::Migration
-  def change
-    remove_column :drivers, :accomodate_pet, :bool
-    remove_column :drivers, :accomodate_aide, :bool
-  end
-end
